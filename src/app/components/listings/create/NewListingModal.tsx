16e5103e4f78c407615786ef19a462326ae1bc31
Mohamed Nainar <nainar.mb@gmail.com>
2023-09-17 10:56:51 +0530

    delete container

---
+++ src/app/components/listings/create/NewListingModal.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { FC } from "react";
+
+interface NewListingModalProps {}
+
+const NewListingModal: FC<NewListingModalProps> = ({}) => {
+  return <div>NewListingModal</div>;
+};
+
+export { NewListingModal };
--- src/app/components/navbar/NavBar.tsx
+++ src/app/components/navbar/NavBar.tsx
@@ -1,8 +1,8 @@
 import { FC } from "react";
-import { Container } from "../ui/Container";
 import { MdBeenhere } from "react-icons/md";
 import { SearchBar } from "./SearchBar";
 import { UserMenu } from "./UserMenu";
+import { NewListingModal } from "../listings/create/NewListingModal";
 import { Dialog, DialogTrigger, DialogContent } from "../ui/Dialog";
 
 import Link from "next/link";
@@ -13,32 +13,33 @@ const NavBar: FC<NavBarProps> = ({}) => {
   // const setOpenLogin = useStore((s) => s.setOpenLogin);
   return (
     <div className="w-full bg-slate-100 py-5 shadow-sm h-20 border-b-[1px] border-slate-300">
-      <Container>
-        <div className="flex flex-row justify-between items-center my-auto h-10 px-6 md:px-16">
-          <Link href="/">
-            <div className="hidden sm:flex flex-row items-center">
-              <MdBeenhere className="hidden sm:block h-10 w-10 text-purple-600" />
-              <div className=" hidden lg:block text-purple-600 text-2xl font-semibold pl-2">
-                NextInn_
-              </div>
+      <div className="flex flex-row justify-between items-center my-auto h-10 px-6 md:px-16">
+        <Link href="/">
+          <div className="hidden sm:flex flex-row items-center">
+            <MdBeenhere
+              className="hidden sm:block h-10 w-10 text-purple-600"
+              aria-label="NextInn logo"
+            />
+            <div className=" hidden lg:block text-purple-600 text-2xl font-semibold pl-2">
+              NextInn_
             </div>
-          </Link>
-          <SearchBar />
-          <div className="flex flex-row justify-end items-center gap-5">
-            <Dialog>
-              <DialogTrigger asChild>
-                <div className="hidden min-w-fit sm:block rounded-full hover:bg-slate-200 px-3 py-1 cursor-pointer">
-                  Become a Host
-                </div>
-              </DialogTrigger>
-              <DialogContent title="Host your Home">
-                <span>hi</span>
-              </DialogContent>
-            </Dialog>
-            <UserMenu />
           </div>
+        </Link>
+        <SearchBar />
+        <div className="flex flex-row justify-end items-center gap-5">
+          <Dialog>
+            <DialogTrigger asChild>
+              <div className="hidden min-w-fit sm:block rounded-full hover:bg-slate-200 px-3 py-1 cursor-pointer">
+                Become a Host
+              </div>
+            </DialogTrigger>
+            <DialogContent title="Host your Home">
+              <NewListingModal />
+            </DialogContent>
+          </Dialog>
+          <UserMenu />
         </div>
-      </Container>
+      </div>
     </div>
   );
 };
